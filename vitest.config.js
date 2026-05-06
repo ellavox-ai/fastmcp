3e1e499a776215ac66c5b286ecf87aa8a0ca1d86
josborn <john.osborn.ii@gmail.com>
2026-05-06 11:50:25 -0500

    bumping deps

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    poolOptions: {
-      forks: { execArgv: ["--experimental-eventsource"] },
-    },
+    execArgv: ["--experimental-eventsource"],
   },
 });
